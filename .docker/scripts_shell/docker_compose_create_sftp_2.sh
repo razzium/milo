fd54e233af1ac1e82c5faca6b85bc71ba3431cbd
Robin Razzi <rrazzi@synertic.fr>
2019-04-19 17:10:04 +0200

    - test optim

--- .docker/scripts_shell/docker_compose_create_sftp_2.sh
+++ .docker/scripts_shell/docker_compose_create_sftp_2.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/usr/local/bin/docker-compose exec sftp-server sh -c "sudo mkdir /uploads/$1/src"
+/usr/local/bin/docker-compose exec sftp-server sh -c "sudo mkdir /uploads/$1/src; sudo chmod -R 777 /uploads/$1/src;"
